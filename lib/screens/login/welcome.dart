dffc884bf03528798fdc7b55e053a3e671a91d02
mariaandreescu <maria.andreescu@stud.acs.upb.com>
2022-02-08 20:32:25 +0200

    removed status bar & bottom

--- lib/screens/login/welcome.dart
+++ lib/screens/login/welcome.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:myfirstapp/screens/login/components/body.dart';
 
 class WelcomeScreen extends StatelessWidget {
@@ -6,6 +7,7 @@ class WelcomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
     return const Scaffold(
       body: Body(),
     );
